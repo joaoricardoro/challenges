e4288582d6451822b97a213bbcfbe87d027ddcf9
João Ricardo <joaoricardoroliveira@gmail.com>
2021-03-22 22:22:06 -0300

    fixing folders

--- dev/src/test/Login.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Login from '../pages/Login/index';
-
-test('renders learn react link', () => {
-  render(<Login />);
-  const linkElement = screen.getByText(/Joao/i);
-  expect(linkElement).toBeInTheDocument();
-});
